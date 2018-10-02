4193d097dcdf031380c6b7bffc93d5334c2f4abb
Oscarchoi <wychoi502@gmail.com>
2018-10-02 16:45:12 +0900

    CleanCode

--- Server_PollManager.cpp
+++ Server_PollManager.cpp
@@ -9,8 +9,7 @@ using namespace std;
 
 
 //생성자
-PollManager::PollManager(int sd): serverfd(sd)
-{
+PollManager::PollManager(int sd): serverfd(sd){
   for(int i=0; i<MAXINST; i++){
     g_pollfd[i] = {-1,POLLIN, 0};
   }
@@ -19,20 +18,15 @@ PollManager::PollManager(int sd): serverfd(sd)
 }
 
 
-
 // Client Manager를 등록함: 0 = 성공, -1 = 실패
 int PollManager::register_ClientManager(ClientManager* ptr){
-  if(ptr == NULL)
-    return -1;
+  if(ptr == NULL) return -1;
   cmptr = (ClientManager*)ptr;
-// cout<<"CM 등록"<<endl;
-//  cout<<cmptr->CSession[0]->get_mysd()<<endl;
-//  cout<<cmptr->CSession[0]->get_myID()<<endl;	
   return 0;
 }
 
 
-// Get Next pollfd Ptr
+// Get Next assigned pollfd Ptr
 struct pollfd* PollManager::get_NextPollfd(struct pollfd* cur){
   int index;
   if(cur==NULL) index = 0;
@@ -40,25 +34,18 @@ struct pollfd* PollManager::get_NextPollfd(struct pollfd* cur){
     index = cur - &g_pollfd[0];
     index++;
   }
-
+  //Search pfd which has fd not equal to -1
   struct pollfd* pfd;
-  //cout<<"Next pollfd index = "<<index<<endl;
-
-
-  for(pfd = &g_pollfd[index]; pfd < pollfd_end;++pfd)
-  {
-   //cout<<"index = "<<pfd-&g_pollfd[index]<<endl;
+  for(pfd = &g_pollfd[index]; pfd < pollfd_end;++pfd){
     if(pfd->fd != -1) return pfd;
   }
   return NULL;
-
 }
 
 // Poll revent handler
 struct pollfd* PollManager::get_NextReventPoll(struct pollfd* pfd){
   struct pollfd* cur = pfd;
   while((cur = get_NextPollfd(cur))){
-    //cout<<"while in"<<endl;
     if(cur->revents == POLLIN){
       return cur;
     }				
@@ -87,30 +74,29 @@ int PollManager::accept_Pollfd(int sd){
 
 // Execute Poll 
 void PollManager::do_Poll(void){
-  struct pollfd* cur = NULL;   
-  nread = poll(g_pollfd, MAXINST, -1); //poll
-  //cout<<"nread = "<<nread<<endl;
-  for (int n_poll = 0; n_poll < nread; n_poll++)  // iterate nread times
-  { 
-    //cout<<"poll loop start"<<endl;
+  while(1){
+    struct pollfd* cur = NULL;    
+    nread = poll(g_pollfd, MAXINST, -1);
+    
+    for (int n_poll = 0; n_poll < nread; n_poll++)  // iterate nread times
+    { 
+      cur = get_NextReventPoll(cur);  // return type : struct pollfd*
+      int index = cur-&g_pollfd[0];
 
-    cur = get_NextReventPoll(cur);  // return type : struct pollfd*
-    int index = cur - &g_pollfd[0];
-    //cout<<"index = "<<index<<endl;
-    if(index == 0)
-    {
-      cout<<"Accept Socket Event Occur!"<<endl;
-      int EmptyPfdIndex = get_EmptyPfdIndex();
-      int nfd = accept_Pollfd(serverfd);	 
-      g_pollfd[EmptyPfdIndex].fd = nfd; // Save new client's sd in emptypfdindex	 
-      cmptr->respond_Poll(EmptyPfdIndex, nfd, 0);
-    } // 
-    else if(index > 0){
-      cout<<"Client Socket Event Occur!"<<endl;
-      cmptr->respond_Poll(index,cur->fd,1); 
-    }
-    else{
-      cout<<"Wrong Index Input Error: sd "<<cur->fd<<endl;      
+      if(index == 0){
+	cout<<"Accept Socket Event Occur!"<<endl;
+	int EmptyPfdIndex = get_EmptyPfdIndex();
+	int nfd = accept_Pollfd(serverfd);	 
+	g_pollfd[EmptyPfdIndex].fd = nfd; // Save new client's sd in emptypfdindex	 
+	cmptr->respond_Poll(EmptyPfdIndex, nfd, 0);
+      }  
+      else if(index > 0){
+	cout<<"Client Socket Event Occur!"<<endl;
+	cmptr->respond_Poll(index,cur->fd,1); 
+      }
+      else{
+	cout<<"Wrong Index Input Error: sd "<<cur->fd<<endl;      
+      }
     }
   }
 }
--- Server_main.cpp
+++ Server_main.cpp
@@ -5,16 +5,12 @@
 using namespace std;
 
 int main(int argc,char* argv[]){
-
   int port_in =  atoi(argv[1]);
   int create_Server(int);
 
-
   //server socket open 
   int sd;
-  if ((sd = create_Server(port_in)) == -1){
-    return -1;
-  }
+  if ((sd = create_Server(port_in)) == -1) return -1;
   cout<<"티맥스 대화방 초기화중..."<<endl;
   
   //PollManager 생성 및 Pollfd 배정
@@ -22,13 +18,9 @@ int main(int argc,char* argv[]){
 
   // Client Manager 생성
   ClientManager CM_1(&PM);
-
   cout<<"티맥스 대화방 시작"<<endl;
 
-  while(1){
-    PM.do_Poll(); //do_POll안에 while 걸기
-  }
-
+  PM.do_Poll(); 
   return 0;
 }
 
@@ -39,7 +31,7 @@ int create_Server(int port)
   struct sockaddr_in server_addr;
   int serverlen = sizeof(sockaddr_in);
   s = socket(AF_INET, SOCK_STREAM, 0);                                    
-  //CHECK(s >= 0);                                                              
+                                                             
   memset((char *)&server_addr, 0, sizeof(server_addr));                       
   server_addr.sin_family = AF_INET;                                           
   server_addr.sin_addr.s_addr = htonl(INADDR_ANY);                            
